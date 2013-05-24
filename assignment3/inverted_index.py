caab11606ee642293d467999050fd535e26d36ca
Brett Gerry <brett@twilio.com>
2013-05-23 18:07:29 -0700

    first pass at problem 1

---
+++ assignment3/inverted_index.py
@@ -0,0 +1,38 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of doc identifiers
+    seen = set()
+    new_list = []
+    for val in list_of_values:
+        if val in seen:
+            continue
+        seen.add(val)
+        new_list.append(val)
+    mr.emit((key, new_list))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
